c8db0e22b35741d6e235f426f2f32d3f983c47c1
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2022-01-19 12:13:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -61,17 +61,28 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = ` 
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+  </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -94,16 +105,124 @@ arr.splice(1, 2); //starts at element at position 1 and removes two elements
 console.log(arr);
 */
 
+/*
 // REVERSE
 let arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
 console.log(arr2.reverse()); //mutates the array
 console.log(arr2);
 
-//CONCAT
+// CONCAT
 const letters = arr.concat(arr2);
 console.log(letters);
 console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' - '));
+*/
+
+/*
+// AT METHOD
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0)); //same as above
+
+//getting the last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1));
+console.log(arr.at(-1));
+
+//also works on strings
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
+
+*/
+
+/*
+// LOOPING ARRAYS: FOREACH
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log('___________FOREACH______________');
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+//(mov, i, arr) 1st current element then index then array
+//you can break out of a FOREACH loop using continue or break you would have to use FOROF
+*/
+/*
+// FOREACH WITH MAPS AND SETS
+//MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+//SET
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique); //only returns unique values
+
+currenciesUnique.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`); //no keys in a set so this will equal each other
+});
+*/
+
+/*Coding Challenge #1
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners
+about their dog's age, and stored the data into an array (one array for each). For
+now, they are just interested in knowing whether a dog is an adult or a puppy.
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years
+old.
+Your tasks:
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages
+('dogsJulia' and 'dogsKate'), and does the following things:
+1. Julia found out that the owners of the first and the last two dogs actually have
+cats, not dogs! So create a shallow copy of Julia's array, and remove the cat
+ages from that copied array (because it's a bad practice to mutate function
+parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1
+is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy
+🐶
+")
+4. Run the function for both test datasets
+Test data:
+§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+Hints: Use tools from all lectures in this section so far 😉
+GOOD LUCK 😀
+*/
+/*
+//const dogsJulia = [3, 5, 2, 12, 7];
+const dogsJulia = [9, 16, 6, 8, 3];
+//const dogsKate = [4, 1, 15, 8, 3];
+const dogsKate = [10, 5, 6, 1, 4];
+const newDogsJulia = dogsJulia.slice(1, -2);
+console.log(newDogsJulia);
+const allDogs = newDogsJulia.concat(dogsKate);
+console.log(allDogs);
+
+const checkDogs = allDogs.forEach(function (dog, i) {
+  dog < 3
+    ? console.log(`Dog number ${i + 1} is still a puppy 🐶`)
+    : console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+});
+*/
